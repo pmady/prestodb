430a40cfb38f38e686b63fb41d4cb7f42ce3387a
mapa9006 <pavan.madduri@nielsen.com>
2020-05-06 15:02:24 -0500

    adding files current repo

--- presto/build_image.sh
+++ presto/build_image.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-REPONAME=joshuarobinson
-PRESTOVER=330
+REPONAME=pmady7389
+PRESTOVERSION=333
 
-docker build --build-arg PRESTO_VER=$PRESTOVER -t fb-presto .
+docker build --build-arg PRESTO_VERSION=$PRESTOVERSION -t prestosql .
 
 # Tag and push to the public docker repository.
-docker tag fb-presto $REPONAME/fb-presto
-docker push $REPONAME/fb-presto
+docker tag fb-presto $REPONAME/prestosql
+docker push $REPONAME/prestosql
 
 # Update configMap in Kubernetes
 kubectl create configmap presto-cfg --dry-run --from-file=config.properties.coordinator --from-file=config.properties.worker --from-file=node.properties.template --namespace=pavan -o yaml | kubectl apply -f -
