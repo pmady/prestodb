45d92a6b1eba57de9cef0e0d013c086c926a7ef1
pmady <cloud_user@lalit2109841c.mylabserver.com>
2020-05-06 20:41:23 +0000

    testing

--- presto/build_image.sh
+++ presto/build_image.sh
@@ -5,12 +5,14 @@ set -e
 REPONAME=pmady7389
 PRESTOVERSION=333
 
-docker build --build-arg PRESTO_VERSION=$PRESTOVERSION -t prestosql .
+#docker build --build-arg PRESTO_VERSION=$PRESTOVERSION -t prestosql .
 
 # Tag and push to the public docker repository.
-docker tag fb-presto $REPONAME/prestosql
-docker push $REPONAME/prestosql
+#docker tag fb-presto $REPONAME/prestosql
+#docker push $REPONAME/prestosql
 
 # Update configMap in Kubernetes
+kubectl create namespace pavan
+kubectl config set-context --current --namespace=pavan
 kubectl create configmap presto-cfg --dry-run --from-file=config.properties.coordinator --from-file=config.properties.worker --from-file=node.properties.template --namespace=pavan -o yaml | kubectl apply -f -
 kubectl apply -f presto.yaml
